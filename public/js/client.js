aa8447f74f61ae2ee91afdbcb498b978bbc7add7
unknown <dinesh9@PRUH-W-DEF-0454.pruflex.com>
2014-02-12 09:00:02 +0000

    Updated presentation

--- public/js/client.js
+++ public/js/client.js
@@ -3,7 +3,10 @@
               Reveal.initialize({
                 history: true
               });
-              Reveal.addEventListener('slidechanged',function(event){event.preventDefault();});
+              Reveal.addEventListener('slidechanged',
+			  function(event){
+			  event.preventDefault();
+			  });
 			  var url=document.URL.split('/');
 				var host=url[0]+'//'+url[2];
               var socket = io.connect(host);
